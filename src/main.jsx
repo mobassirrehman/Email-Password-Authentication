c0bbb3e8583fa054c6d2d24022a62160688e3414
Mobassir Rehman <mobassir.rehman@Mobassirs-MacBook-Pro.local>
2025-10-17 23:42:44 +0600

    project set-up and navbar

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from "./layout/Root";
+import Home from "./Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
