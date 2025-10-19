571073bf3ad417aeb210062e4e4f3f42b5241ff7
Mobassir Rehman <mobassir.rehman@Mobassirs-MacBook-Pro.local>
2025-10-19 21:43:15 +0600

    login user & toggle registration & login route

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { Link } from "react-router";
+import { auth } from "../../firebase/firebase.init";
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [error, setError] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log("Login submitted", email, password);
+
+    setError("");
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log("User logged in:", userCredential.user);
+      })
+      .catch((error) => {
+        console.log("Error during login:", error);
+        setError(error.message);
+      });
+  };
+  return (
+    <div className="card bg-base-100 mt-6 w-full max-w-sm m-auto shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h2 className="text-3xl m-auto font-bold">Login now!</h2>
+        <form onSubmit={handleLogin}>
+          <fieldset className="fieldset">
+            <label className="label">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="Email"
+            />
+            <label className="label">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="input"
+              placeholder="Password"
+            />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+        </form>
+        {error && <p className="text-red-600 mt-4">{error}</p>}
+        <p>
+          First time here? Please{" "}
+          <Link className="text-blue-600 underline" to="/register">
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { auth } from "../../firebase/firebase.init";
 import { IoMdEye } from "react-icons/io";
 import { IoMdEyeOff } from "react-icons/io";
+import { Link } from "react-router";
 
 const Register = () => {
   const [success, setSuccess] = useState(false);
@@ -13,7 +14,8 @@ const Register = () => {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
-    console.log("Form submitted", email, password);
+    const termsAccepted = event.target.terms.checked;
+    console.log("Form submitted", email, password, termsAccepted);
 
     const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$&*]).{6,}$/;
     const casePattern = /[a-z]/;
@@ -25,6 +27,10 @@ const Register = () => {
       return;
     } else if (!casePattern.test(password)) {
       setError("Password must contain at least one lowercase letter.");
+      return;
+    }
+    if (!termsAccepted) {
+      setError("You must accept the Terms & Conditions to register.");
       return;
     }
 
@@ -51,11 +57,10 @@ const Register = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Register now!</h1>
-        </div>
+        <div className="text-center lg:text-left"></div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
+            <h2 className="text-3xl m-auto font-bold">Register now!</h2>
             <form onSubmit={handleRegister}>
               <fieldset className="fieldset">
                 <label className="label">Email</label>
@@ -81,8 +86,8 @@ const Register = () => {
                   </button>
                 </div>
                 <label className="label">
-                  <input type="checkbox" className="checkbox" />
-                  Remember me
+                  <input type="checkbox" name="terms" className="checkbox" />
+                  Accept Terms & Conditions
                 </label>
                 <div>
                   <a className="link link-hover">Forgot password?</a>
@@ -96,6 +101,12 @@ const Register = () => {
               )}
               {error && <p className="text-red-700 mt-2">{error}</p>}
             </form>
+            <p>
+              Already have an account? Please{" "}
+              <Link className="text-blue-600 underline" to="/login">
+                Login
+              </Link>
+            </p>
           </div>
         </div>
       </div>
